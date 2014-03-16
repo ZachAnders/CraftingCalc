2e14a12c509b6e415cf71b7e3adda4eae10486d6
ZachAnders <Zacharia.Anders@gmail.com>
2014-03-15 23:08:24 -0600

    Changed dev/prod again

--- Application.py
+++ Application.py
@@ -190,8 +190,11 @@ def pass_time():
 app.secret_key = os.urandom(128)
 
 if __name__ == "__main__":
-	if not os.path.exists("./production"):
+	if "PRODUCTION" in os.environ and bool(os.environ["PRODUCTION"]):
+		print("Environment: Production")
+	else:
+		print("Environment: Development")
 		app.debug = True
-	if app.debug:
 		app.secret_key = '\xe5\x1e\xe8\xc7h\xccr\x9c7\xee|dN\x85\x8c{-4<\xa5\xe5\x03\xc7?\x16\xc3\x181+\xab\xf3q'
 	app.run()
+
--- crafting_calc.wsgi
+++ crafting_calc.wsgi
@@ -1,4 +1,5 @@
-import sys, site
+import sys, site, os
 sys.path.insert(0, '/var/www/calc/craftingcalc/CraftingCalc')
 site.addsitedir("/var/www/calc/craftingcalc/lib/python2.7/site-packages")
+os.environ["PRODUCTION"] = str(True)
 from Application import app as application
